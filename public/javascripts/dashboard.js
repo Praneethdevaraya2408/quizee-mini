e6d5eded5f82fc9e80dafb5ada8598d88dac91f2
Praneethdevaraya2408 <praneethdevaraya2408@gmail.com>
2021-07-02 20:40:57 -0700

    timer done

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,5 +1,5 @@
 import userObject from './ls.js';
-
+$("#app").hide();
 $( document ).ready( function ()
 {
     $( '#Logoutbtn' ).click( function ()
@@ -13,7 +13,7 @@ $( document ).ready( function ()
         location.href = "/";
     } );
 
-    
+    $("#app").hide();  
     var createquiz = false
     var cnt = 1
     var quizlength = 0
@@ -155,6 +155,9 @@ $( document ).ready( function ()
                                     console.log( data.quiz );
                                     console.log( data.quiz.quizName );
                                     //console.log(data.quiz.questions.length);
+                                    $("#app").show();
+                                    var duration=data.quiz.quizDuration;
+                                    timer(duration)
                                     quizlength = data.quiz.questions.length;
                                     quiz_Code = data.quiz._id;
                                     $( "#quiztitle" ).html( data.quiz.quizName );
@@ -343,3 +346,122 @@ $( document ).ready( function ()
     });
 });
 
+var timer=function(duration){
+    const TIME_LIMIT = 60*duration; // Give time here
+    const FULL_DASH_ARRAY = 283;
+    const WARNING_THRESHOLD = 0.40*TIME_LIMIT;
+    const ALERT_THRESHOLD = 0.15*TIME_LIMIT;
+    
+    const COLOR_CODES = {
+      info: {
+        color: "green"
+      },
+      warning: {
+        color: "orange",
+        threshold: WARNING_THRESHOLD
+      },
+      alert: {
+        color: "red",
+        threshold: ALERT_THRESHOLD
+      }
+    };
+    
+    let timePassed = 0;
+    let timeLeft = TIME_LIMIT;
+    let timerInterval = null;
+    let remainingPathColor = COLOR_CODES.info.color;
+    
+    document.getElementById("app").innerHTML = `
+    <div class="base-timer">
+      <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <g class="base-timer__circle">
+          <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+          <path
+            id="base-timer-path-remaining"
+            stroke-dasharray="283"
+            class="base-timer__path-remaining ${remainingPathColor}"
+            d="
+              M 50, 50
+              m -45, 0
+              a 45,45 0 1,0 90,0
+              a 45,45 0 1,0 -90,0
+            "
+          ></path>
+        </g>
+      </svg>
+      <span id="base-timer-label" class="base-timer__label">${formatTime(
+        timeLeft
+      )}</span>
+    </div>
+    `;
+    
+    startTimer();
+    
+    function onTimesUp() {
+      clearInterval(timerInterval);
+      console.log("time up");
+      
+      
+    }
+    
+    function startTimer() {
+      timerInterval = setInterval(() => {
+        timePassed = timePassed += 1;
+        timeLeft = TIME_LIMIT - timePassed;
+        document.getElementById("base-timer-label").innerHTML = formatTime(
+          timeLeft
+        );
+        setCircleDasharray();
+        setRemainingPathColor(timeLeft);
+    
+        if (timeLeft === 0) {
+          onTimesUp();
+          $('#submitquizbutton').trigger('click');
+        }
+      }, 1000);
+    }
+    
+    function formatTime(time) {
+      const minutes = Math.floor(time / 60);
+      let seconds = time % 60;
+    
+      if (seconds < 10) {
+        seconds = `0${seconds}`;
+      }
+    
+      return `${minutes}:${seconds}`;
+    }
+    
+    function setRemainingPathColor(timeLeft) {
+      const { alert, warning, info } = COLOR_CODES;
+      if (timeLeft <= alert.threshold) {
+        document
+          .getElementById("base-timer-path-remaining")
+          .classList.remove(warning.color);
+        document
+          .getElementById("base-timer-path-remaining")
+          .classList.add(alert.color);
+      } else if (timeLeft <= warning.threshold) {
+        document
+          .getElementById("base-timer-path-remaining")
+          .classList.remove(info.color);
+        document
+          .getElementById("base-timer-path-remaining")
+          .classList.add(warning.color);
+      }
+    }
+    
+    function calculateTimeFraction() {
+      const rawTimeFraction = timeLeft / TIME_LIMIT;
+      return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+    }
+    
+    function setCircleDasharray() {
+      const circleDasharray = `${(
+        calculateTimeFraction() * FULL_DASH_ARRAY
+      ).toFixed(0)} 283`;
+      document
+        .getElementById("base-timer-path-remaining")
+        .setAttribute("stroke-dasharray", circleDasharray);
+    }
+    }
